7aba55982586981d2056e92303ca2702b0ca7ba7
SantiagoFigueroaMc <sjfigueroa@miuandes.cl>
2018-06-02 17:52:55 -0400

    Vertice list

--- main.cpp
+++ main.cpp
@@ -2,24 +2,28 @@
 # include "vertice.h"
 # include "arista.h"
 
-// Cree un arco entre los nodos d y h, donde la dirección es desde d a h y el peso es w.
+// Crea un arco entre los nodos d y h, donde la dirección es desde d a h y el peso es w.
 void add_arco(int d, int h, int w){
 
 }
 
-// Agregue un nodo y retorne el número que lo identifica. Los nodos serán identificados por un número secuencial que comenzará en cero.
+// Agregua un nodo y retorne el número que lo identifica. Los nodos serán identificados por un número secuencial que comenzará en cero.
 int add_nodo(){
 
 }
 
-// Retorne true si existe un arco entre d y h, en la dirección (d ->h) o false en caso contrario.
+// Retorna true si existe un arco entre d y h, en la dirección (d ->h) o false en caso contrario.
 bool hay_arco(int d, int h){
 
 }
 
+// Retorna true si existe un camino entre d y h o false en caso contrario.
+bool hay_camino(int d, int h){
+
+}
 
 int main (){
-
+	
 	return 0;
 }
 
--- vertice.cpp
+++ vertice.cpp
@@ -1,2 +1,15 @@
 #include "vertice.h"
 
+// Constructor:
+Vertice::Vertice(int mi_name){
+    name = mi_name;
+}
+
+// Metodos:
+int Vertice::GetName(){
+    return name;
+}
+
+static int Vertice::GetN_Vertice(){
+    return n_vertice;
+}
