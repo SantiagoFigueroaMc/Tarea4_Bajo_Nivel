7a2868d25224c8518b68fd0fd789554188d925f4
SantiagoFigueroaMc <sjfigueroa@miuandes.cl>
2018-06-02 17:32:53 -0400

    Main funciones

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
+# include "vertice.h"
+# include "arista.h"
+
+// Cree un arco entre los nodos d y h, donde la dirección es desde d a h y el peso es w.
+void add_arco(int d, int h, int w){
+
+}
+
+// Agregue un nodo y retorne el número que lo identifica. Los nodos serán identificados por un número secuencial que comenzará en cero.
+int add_nodo(){
+
+}
+
+// Retorne true si existe un arco entre d y h, en la dirección (d ->h) o false en caso contrario.
+bool hay_arco(int d, int h){
+
+}
+
 
 int main (){
+
 	return 0;
 }
+
+
