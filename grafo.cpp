f799c6c8e3107e938a1a67d43448d72a12ae0a17
Thecrashbandicoot36 <atcereceda@miuandes.cl>
2018-06-02 19:27:17 -0400

    separando los archivos

--- grafo.cpp
+++ grafo.cpp
@@ -1,2 +1,22 @@
 # include "grafo.h"
 
+// Crea un arco entre los nodos d y h, donde la dirección es desde d a h y el peso es w.
+void add_arco(int d, int h, int w){
+
+}
+
+// Agregua un nodo y retorne el número que lo identifica. Los nodos serán identificados por un número secuencial que comenzará en cero.
+int add_nodo(){
+	return 0;
+}
+
+// Retorna true si existe un arco entre d y h, en la dirección (d ->h) o false en caso contrario.
+bool hay_arco(int d, int h){
+	return true;
+}
+
+// Retorna true si existe un camino entre d y h o false en caso contrario.
+bool hay_camino(int d, int h){
+	return true;
+}
+
--- grafo.h
+++ grafo.h
@@ -1,6 +1,13 @@
 #ifndef _GRAFO
 #define _GRAFO
 class Grafo {
+private:
+	//vectores
+public:
+	void add_arco(int , int , int );
+	int add_nodo();
+	bool hay_arco(int , int );
+	bool hay_camino(int , int );
 
 };
 
