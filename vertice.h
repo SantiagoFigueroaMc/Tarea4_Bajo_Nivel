741780ec8b9e907a9c6c162e2fd9b7efabf8191a
SantiagoFigueroaMc <sjfigueroa@miuandes.cl>
2018-06-02 17:26:49 -0400

    Vertice

---
+++ vertice.h
@@ -0,0 +1,16 @@
+#ifndef _VERTICE
+#define _VERTICE
+class Vertice (){
+private:
+    int name;
+    static int n_vertice;
+public:
+    // Constructor:
+    Vertice(int);
+
+    // Metodos:
+    int GetName();
+    static int GetN_Vertice();
+}
+
+#endif
