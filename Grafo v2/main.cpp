1abb246b28f6dfcf9b2948345775c22114967e1d
SantiagoFigueroaMc <sjfigueroa@miuandes.cl>
2018-06-10 11:21:20 -0400

    Un main mas atrevido...

--- Grafo v2/main.cpp
+++ Grafo v2/main.cpp
@@ -13,12 +13,14 @@ int main(){
     /*  Prueba de grafo     */
     Grafo mapa_1 (24500);//Llamada al constructor.
     cout << mapa_1.GetName() << endl;
-    mapa_1.AddNodo();
-    mapa_1.AddNodo();
-    mapa_1.AddNodo();
+    for (int cantidad_de_nodos = 0; cantidad_de_nodos < 15; cantidad_de_nodos++){
+        mapa_1.AddNodo();
+    }
     cout << "Nodos en la red: " << mapa_1.PrintAllNodes() << endl;
-    cout << "Se borrara el nodo 2." << endl;
+    cout << "Se borrara el nodo 2, 6 y 10." << endl;
     mapa_1.DeleteNodo(2);
+    mapa_1.DeleteNodo(6);
+    mapa_1.DeleteNodo(10);
     cout << "Nodos en la red: " << mapa_1.PrintAllNodes() << endl;
     cout << "Se agregara un nuevo nodo en la red." << endl;
     mapa_1.AddNodo();
