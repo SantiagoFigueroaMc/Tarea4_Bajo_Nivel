d60ed5f61bdfb914bed32c2ad48b7a6d32795a28
SantiagoFigueroaMc <sjfigueroa@miuandes.cl>
2018-06-10 11:05:14 -0400

    Arreglos en add_path y nueva funcion: remove_path

--- Grafo v2/node.cpp
+++ Grafo v2/node.cpp
@@ -7,9 +7,24 @@ Node::Node(){
 };
 
 //Metodo para agregar un camino a un nodo n.
-void Node::Add_path(Node n){
+bool Node::Add_path(Node n){
 	conexiones.push(n);
+	numero_conexiones++;
+	return true;
 };
+
+bool Node::Remove_path(Node n){
+	int conexiones_actuales = numero_conexiones;
+	for(int temp_node = 0; temp_node < numero_conexiones; temp_node++)
+	{
+		if (conexiones.at(temp_node).GetId() == n.GetId()){
+			conexiones.erase(conexiones.begin() + temp_node)
+			numero_conexiones--;
+			return true;
+		}
+	}
+	return false;
+}
 
 int Node::GetId(){
 	return id;
@@ -18,3 +33,7 @@ int Node::GetId(){
 queue<Node> Node::GetConexiones(){
 	return conexiones;
 };
+
+int Node::GetNumber_of_conections(){
+	return numero_conexiones;
+}
--- Grafo v2/node.h
+++ Grafo v2/node.h
@@ -15,15 +15,19 @@ public:
 //Atributos publicos:
 //  Tipo:      	Nombre:
 	static int 	numero_nodos;
+	int			numero_conexiones;
 
 //Constructor:
 	Node();
 
 //Metodos:
 //  Tipo:     		Nombre:
-	void 			Add_path(Node);
+	bool 			Add_path(Node);// Grafo le entrega el nodo
+	bool			Remove_path(Node);// Quita al nodo n de las conexiones de esta clase
 	int 			GetId();
 	queue<Node> 	GetConexiones();
+	int				GetNumber_of_conections();// Puede servir para ver al nodo mas popular, lo que hablamos de los arboles
+
 };
 
 #endif
