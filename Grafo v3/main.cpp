400fe5d3d2e22149d8087d08b28a4387874c7180
SantiagoFigueroaMc <sjfigueroa@miuandes.cl>
2018-06-11 17:02:34 -0400

    .

--- Grafo v3/main.cpp
+++ Grafo v3/main.cpp
@@ -10,19 +10,6 @@ using namespace std;
 int Node::numero_nodos = 0;
 int main(){
     cout << "\nInicio programa." << endl;
-    /*  Prueba de grafo     */
-    Grafo mapa_1 (24500);//Llamada al constructor.
-    cout << mapa_1.GetName() << endl;
-    for (int cantidad_de_nodos = 0; cantidad_de_nodos < 15; cantidad_de_nodos++){
-        cout << mapa_1.AddNodo() << endl;
-    }
-    cout << "Nodos en la red: " << mapa_1.GetCantidad_nodos() << endl;
-    mapa_1.AddConexion(1,13, 24);
-    cout << "Se creo un camino de 1 a 13, con peso 24" << endl;
-    if (mapa_1.hay_arco(1, 13)){
-        cout << "Hay camino de 1 a 13" << endl;
-    }
     
-    //cout << mapa_1.PrintAllConexions(1) << endl;
     cout << "Fin programa.\n\n"<<endl;
 }
