94835464f939b0b4e799b3009e593d0ed3347eb6
Thecrashbandicoot36 <atcereceda@miuandes.cl>
2018-06-10 23:38:01 -0400

    no debiera cambiarse nada

--- Grafo v2/grafo.h
+++ Grafo v2/grafo.h
@@ -16,18 +16,21 @@ private:
     int             cantidad_nodos;
 public:
 //Constructor:
+//  Tipo:           Nombre:
 	//falta el constructor vacío
     Grafo(int);// recibe el nombre del grafo
 //Metodos:
 //  Tipo:           Nombre:
     void            AddConexion(int, int, int); // se le entrega el nodo de donde parte el camino, el nodo a unir y el peso
     int             AddNodo();                  // se crea y agrega un nodo
-    bool            Hay_Arco(int, int);      // revisa si hay camino de A a B. A-->B
     bool            GetAdventureMap(int,int);   // busca un camino entre el primer int y el segundo, que corresponden a nodos
 
     Node            GetNodo(int);               // se le pasa el id del nodo y devuelve el nodo en la posicion int
     int             GetName();
     int             GetCantidad_nodos();
+//Tarea:
+//  Tipo:           Nombre:
+    bool            hay_arco(int, int);      // revisa si hay camino de A a B. A-->B
 };
 
 #endif
