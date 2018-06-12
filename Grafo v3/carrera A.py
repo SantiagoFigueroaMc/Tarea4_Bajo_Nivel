fc47d4487c4f5437aa63b8ed99ee0d25fe29f685
Thecrashbandicoot36 <atcereceda@miuandes.cl>
2018-06-12 00:55:23 -0400

    funciona el hay camino en python (46 lineas por implementar)

--- Grafo v3/carrera A.py
+++ Grafo v3/carrera A.py
@@ -14,7 +14,7 @@ Esquema general:
 	Clase Nodo:
 		
 """
-contadorexterno=0
+
 #Grafo::hay_camino(int I,int F){}
 nodo0 = [ [int("1"),20,"B","desde A"] ]
 nodo1 = [ [int("2"),50,"C","desde B"],[int("0"),58,"A","desde B"],[int("3"),5,"D","desde B"] ]
@@ -22,8 +22,95 @@ nodo2 = [ [int("1"),30,"B","desde C"],[int("3"),48,"D","desde C"] ]
 nodo3 = [ [int("1"),14,"B","desde D"],[int("2"),32,"C","desde D"] ]
 caminos=[]
 graf=[nodo0,nodo1,nodo2,nodo3,caminos]#clase grafo
+
+"""The new and hackerman version"""
+MAX_iterations=50
+def cambiodenodoactual(nodoactual,stack,caminos,F,iteracion,grafo=graf):
+	""" -- -- o -- -- """
+	newlen=len(nodoactual)
+	c3=0		#empeiza el recorrido
+	while (c3<newlen):#parte cambiandose de nodo
+		newnodo=grafo[nodoactual[c3][0]]#nuevo nodo actual
+		stack.append(newnodo)			#agrega el nuevo nodo al camino
+		if (newnodo==grafo[F]):			#se termino el camino
+			caminos+=stack				#se agrega el camino correcto
+			stack.pop(-1)				#debo cambiar la ista de nodos por los que he pasado
+										#cambio newnodo a traves de nodoactual
+		else:	# Seccion:"¿cambio de nodo?")
+			c2=0
+			newlenstack=len(stack)-1
+			cond=True
+			while (c2<newlenstack):
+				if (newnodo==stack[c2]):#reviso que no haya pasado por aqui
+					stack.pop(-1)		#estaba repetido en el camino
+					cond=False			#no debo recursear
+					break
+				c2+=1
+			if cond:	#llamate de nuevo
+				iteracion+=1
+				if iteracion==MAX_iterations+1:
+					break
+				stack,caminos,iteracion=cambiodenodoactual(newnodo,stack,caminos,F,iteracion,grafo=graf)#comienza de nuevo
+		c3+=1	 #si no me cambie de nodo, avanzo a revisar la siguiente seccion
+	stack.pop(-1)#debo sacar del camino el ultimo al ultimo newnodo que se uso
+	return stack,caminos,iteracion	#como finzlizar la recursividad :D
+
+def hay_camino( I, F,grafo=graf):#int I, int F
+	caminos=[]
+	if I==F:#si se busca a si mismo, no se encontrara...no esta listo para saber 0_0
+		return False
+	#Condiciones iniciales
+	nodoActual=grafo[I]
+	stack=[nodoActual]
+	iteracion=1#contador de iteraciones, aaaalgo de worries por la memoria
+	stack,caminos,iteracion=cambiodenodoactual(nodoActual,stack,caminos,F,iteracion,grafo=graf)#entero hackerman
+	#por ser recursivo, lo limitaré a 50 o 100 llamadas (valor MAX_iterations)
+	if iteracion>MAX_iterations:
+		print ("No se puede hacer un camino mas largo")
+	return caminos!=[]#la funcion debe retornar un bool jiji
+#fin del def
+""" En tan solo 71-26 lineas (46 lineas...es menos que 50 sabes?) """
+"""
+nodo0 = [ [int("1"),20,"B","desde A"] ]
+nodo1 = [ [int("2"),50,"C","desde B"],[int("0"),58,"A","desde B"],[int("3"),5,"D","desde B"] ]
+nodo2 = [ [int("1"),30,"B","desde C"],[int("3"),48,"D","desde C"] ]
+nodo3 = [ [int("1"),14,"B","desde D"],[int("2"),32,"C","desde D"] ]
+"""
+print ("desde el nodo 0")
+print (hay_camino(0,0))
+print (hay_camino(0,1))
+print (hay_camino(0,2))
+print (hay_camino(0,3))
+print ("desde el nodo 1")
+print (hay_camino(1,0))
+print (hay_camino(1,1))
+print (hay_camino(1,2))
+print (hay_camino(1,3))
+print ("desde el nodo 2")
+print (hay_camino(2,0))
+print (hay_camino(2,1))
+print (hay_camino(2,2))
+print (hay_camino(2,3))
+print ("desde el nodo 3")
+print (hay_camino(3,0))
+print (hay_camino(3,1))
+print (hay_camino(3,2))
+print (hay_camino(3,3))
+
+
+
+
+"""posible implementacion:
+nodo0 = [ ((["1",20]),caminos=[...]) ]
+nodo1 = [ ((["2",50],["0",58],["3",5]),caminos=[...]) ]
+nodo2 = [ ((["1",30],["3",48]),caminos=[...]) ]
+nodo3 = [ ((["1",14],["2",32]),caminos=[...]) ]
+nodo_sin_conexion = [ ((),caminos=[]) ]
+"""
+
 """ old version
 #como nodo es una clase, una opcion es agregar los caminos dentro del nodo
+contadorexterno=0
 def cambiodenodo(caminos,nActual,stack,F,contadorexterno,grafo=graf):
 	#print ("camino en cambio de nodo:\t"+str(contadorexterno)+"--------")
 	#print (stack)
@@ -144,126 +231,4 @@ def hay_camino(I,F,grafo=graf):#return bool
 #frogging indentation
 
 old version """
-
-def cambiodenodoactual(nodoactual,stack,caminos,F,grafo=graf):
-	""" -- -- o -- -- """
-	newlen=len(nodoactual)
-	c3=0
-	while (c3<newlen):
-#		print "stack actual?"
-#		print (stack)
-		newnodo=grafo[nodoactual[c3][0]]#nuevo nodo actual
-#		print ("revisando nodo:")
-#		print (newnodo)
-		stack.append(newnodo)
-		if (newnodo==grafo[F]):
-			caminos+=stack
-	#						print ("se hizo append del camino")
-	#						print (stack)
-			stack.pop(-1)#debo cambiar la ista de nodos por los que he pasado
-			#cambio newnodo a traves de nodoactual
-			#se termino el camino
-
-		else:#me cambio de nodo
-	#						print ("¿cambio de nodo2?")
-			c2=0
-			newlenstack=len(stack)-1
-			cond=True
-			#			print ("siempre tira True: "+str(nodoactual==stack[lenstack]))
-			while (c2<newlenstack):
-				if (newnodo==stack[c2]):#reviso que no haya pasado por aqui
-					stack.pop(-1)#estaba repetido en el camino
-					cond=False
-					break
-				c2+=1
-			if cond:
-				#llamate de nuevo
-				stack,caminos=cambiodenodoactual(newnodo,stack,caminos,F,grafo=graf)
-		c3+=1
-	stack.pop(-1)
-	return stack,caminos
-
-def hay_camino( I, F,grafo=graf):#int I, int F
-	caminos=[]
-#	print ("\t---\ten busqueda de:"+str(I)+" vs "+str(F))
-#	print (grafo[F])
-	if I==F:
-		return False
-	nodoActual=grafo[I]
-	stack=[nodoActual]
-	c=0
-	lenactual=len(nodoActual)
-#	print (str(lenactual)+"<-len,c->"+str(c))
-	while c<(lenactual):
-		nodoactual=grafo[nodoActual[c][0]]#nodoActual
-#		print (nodoactual)
-		stack.append(nodoactual)#agrego el nuevo nodo a la lista de revisiones
-		#=>nodoActual=nodactual
-		if (nodoactual==grafo[F]):
-			caminos+=stack
-#			print ("se hizo append del camino")
-#			print (stack)
-			stack.pop(-1)#debo cambiar de nodo por el que voy, no me cambio a traves de nodactual
-			#sino a traves de nodoActual
-			#se termino el camino
-		
-		else:#me cambio de nodo
-#			print ("¿cambio de nodo1?")
-			c2=0
-			lenstack=len(stack)-1
-			cond=True
-#			print ("siempre tira True: "+str(nodoactual==stack[lenstack]))
-			while (c2<lenstack):
-				if (nodoactual==stack[c2]):#reviso que no haya pasado por aqui
-					stack.pop(-1)#estaba repetido en el camino
-					cond=False
-					break
-				c2+=1
-			if cond:
-				#debo cambiarme de nodoactual
-#				funcion se llama a si misma
-				stack,caminos=cambiodenodoactual(nodoactual,stack,caminos,F,grafo=graf)
-		c+=1
-#	print("caminos:")
-#	print(caminos)
-#	print "\n"
-	return caminos!=[]
-#fin del def
-"""
-nodo0 = [ [int("1"),20,"B","desde A"] ]
-nodo1 = [ [int("2"),50,"C","desde B"],[int("0"),58,"A","desde B"],[int("3"),5,"D","desde B"] ]
-nodo2 = [ [int("1"),30,"B","desde C"],[int("3"),48,"D","desde C"] ]
-nodo3 = [ [int("1"),14,"B","desde D"],[int("2"),32,"C","desde D"] ]
-"""
-print ("desde el nodo 0")
-print (hay_camino(0,0))
-print (hay_camino(0,1))
-print (hay_camino(0,2))
-print (hay_camino(0,3))
-print ("desde el nodo 1")
-print (hay_camino(1,0))
-print (hay_camino(1,1))
-print (hay_camino(1,2))
-print (hay_camino(1,3))
-print ("desde el nodo 2")
-print (hay_camino(2,0))
-print (hay_camino(2,1))
-print (hay_camino(2,2))
-print (hay_camino(2,3))
-print ("desde el nodo 3")
-print (hay_camino(3,0))
-print (hay_camino(3,1))
-print (hay_camino(3,2))
-print (hay_camino(3,3))
-
-
-
-
-"""posible implementacion:
-nodo0 = [ ((["1",20]),caminos=[...]) ]
-nodo1 = [ ((["2",50],["0",58],["3",5]),caminos=[...]) ]
-nodo2 = [ ((["1",30],["3",48]),caminos=[...]) ]
-nodo3 = [ ((["1",14],["2",32]),caminos=[...]) ]
-nodo_sin_conexion = [ ((),caminos=[]) ]
-"""
 
